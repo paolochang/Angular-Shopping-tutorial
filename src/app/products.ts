d57b5ec90feeadf089a520dfe82d0c906a81c091
Paolo Chang <chanhwan.chang@gmail.com>
2020-05-06 00:10:37 -0400

    Add billing entries

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,17 +1,17 @@
 export const products = [
   {
     name: 'Phone XL',
-    price: 799,
+    price: 799.99,
     description: 'A large phone with one of the best screens'
   },
   {
     name: 'Phone Mini',
-    price: 699,
+    price: 699.99,
     description: 'A great phone with one of the best cameras'
   },
   {
     name: 'Phone Standard',
-    price: 299,
+    price: 299.99,
     description: ''
   }
 ];
